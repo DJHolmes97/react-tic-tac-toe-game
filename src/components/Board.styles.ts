2cb8fdca383e1145d1776b01f900ac259ebd690b
Daniel Holmes <daniel.holmes97@hotmail.com>
2022-09-20 16:18:03 +0100

    Created board initial setup

--- src/components/Board.styles.ts
+++ src/components/Board.styles.ts
@@ -1,5 +1,21 @@
 import styled from 'styled-components'
+import { Container, Row } from 'react-bootstrap'
 
 export const Square = styled.div`
 	background-color: yellow;
+	aspect-ratio: 1 / 1;
+	width: 100%;
+`
+
+export const BoardRow = styled(Row)`
+	height: calc(100% / 3);
+	align-items: center;
+`
+
+export const BoardContainer = styled(Container)`
+	max-width: 500px;
+	min-width: 250px;
+	aspect-ratio: 1 / 1;
+	background-color: blue;
+	--bs-gutter-x: 0.5rem;
 `
