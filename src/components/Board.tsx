80ce94552eae729908055cc9b02fb3c9465987d9
Daniel Holmes <daniel.holmes97@hotmail.com>
2022-09-23 18:23:47 +0100

    WOah the game works using only hooks

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,45 +1,8 @@
+import * as React from 'react'
 import * as Styled from './Board.styles'
 import { Circle, X } from 'react-feather'
-
-interface GamePropTypes {
-	playerOneTurn: boolean
-	boardCells: {
-		a1: 'nought' | 'cross' | 'empty'
-		a2: 'nought' | 'cross' | 'empty'
-		a3: 'nought' | 'cross' | 'empty'
-		b1: 'nought' | 'cross' | 'empty'
-		b2: 'nought' | 'cross' | 'empty'
-		b3: 'nought' | 'cross' | 'empty'
-		c1: 'nought' | 'cross' | 'empty'
-		c2: 'nought' | 'cross' | 'empty'
-		c3: 'nought' | 'cross' | 'empty'
-	}
-}
-enum CellEnum {
-	A1 = 'a1',
-	A2 = 'a2',
-	A3 = 'a3',
-	B1 = 'b1',
-	B2 = 'b2',
-	B3 = 'b3',
-	C1 = 'c1',
-	C2 = 'c2',
-	C3 = 'c3',
-}
-const GameProps: GamePropTypes = {
-	playerOneTurn: true,
-	boardCells: {
-		a1: 'empty',
-		a2: 'empty',
-		a3: 'empty',
-		b1: 'empty',
-		b2: 'empty',
-		b3: 'empty',
-		c1: 'empty',
-		c2: 'empty',
-		c3: 'empty',
-	},
-}
+import { useState } from 'react'
+import { BoardPropTypes, CellEnum } from '../types/gameTypes'
 
 const Square = ({ icon }: { icon: 'nought' | 'cross' | 'empty' }) => {
 	const IconProps = {
@@ -63,22 +26,40 @@ const Square = ({ icon }: { icon: 'nought' | 'cross' | 'empty' }) => {
 	)
 }
 
-const Board = () => {
+const Board = ({
+	gameState,
+	setGameState,
+	gameWinnerCheckHelper,
+	winsCount,
+}: BoardPropTypes) => {
 	const handleClick = (event: any) => {
 		const key: CellEnum = event.currentTarget.attributes['data-id'].value
-		console.log('', key)
-		const oldValue = GameProps.boardCells[key]
-		GameProps.boardCells[key] =
-			GameProps.boardCells[key] === 'empty'
-				? GameProps.playerOneTurn
-					? 'nought'
-					: 'cross'
-				: GameProps.boardCells[key]
-		GameProps.playerOneTurn =
-			oldValue !== GameProps.boardCells[key]
-				? !GameProps.playerOneTurn
-				: GameProps.playerOneTurn
-		console.log(GameProps.boardCells)
+
+		if (
+			gameState.boardCells[key] === 'empty' &&
+			!gameState.winners.player1 &&
+			!gameState.winners.player2 &&
+			gameState.turnCount < 9
+		) {
+			if (gameState.playerOneTurn) {
+				gameState.boardCells[key] = 'nought'
+				gameState.playerOneTurn = false
+				setGameState((gameState) => ({
+					...gameState,
+				}))
+			} else {
+				gameState.boardCells[key] = 'cross'
+				gameState.playerOneTurn = true
+				setGameState((gameState) => ({
+					...gameState,
+				}))
+			}
+			gameWinnerCheckHelper(gameState, setGameState, winsCount)
+			gameState.turnCount++
+			setGameState((gameState) => ({
+				...gameState,
+			}))
+		}
 	}
 	return (
 		<Styled.BoardContainer>
@@ -88,21 +69,21 @@ const Board = () => {
 					onClick={handleClick}
 					data-id={CellEnum.A1}
 				>
-					<Square icon={GameProps.boardCells.a1} />
+					<Square icon={gameState.boardCells.a1} />
 				</Styled.BoardCol>
 				<Styled.BoardCol
 					xs={4}
 					onClick={handleClick}
 					data-id={CellEnum.A2}
 				>
-					<Square icon={GameProps.boardCells.a2} />
+					<Square icon={gameState.boardCells.a2} />
 				</Styled.BoardCol>
 				<Styled.BoardCol
 					xs={4}
 					onClick={handleClick}
 					data-id={CellEnum.A3}
 				>
-					<Square icon={GameProps.boardCells.a3} />
+					<Square icon={gameState.boardCells.a3} />
 				</Styled.BoardCol>
 			</Styled.BoardRow>
 			<Styled.BoardRow>
@@ -111,21 +92,21 @@ const Board = () => {
 					onClick={handleClick}
 					data-id={CellEnum.B1}
 				>
-					<Square icon={GameProps.boardCells.b1} />
+					<Square icon={gameState.boardCells.b1} />
 				</Styled.BoardCol>
 				<Styled.BoardCol
 					xs={4}
 					onClick={handleClick}
 					data-id={CellEnum.B2}
 				>
-					<Square icon={GameProps.boardCells.b2} />
+					<Square icon={gameState.boardCells.b2} />
 				</Styled.BoardCol>
 				<Styled.BoardCol
 					xs={4}
 					onClick={handleClick}
 					data-id={CellEnum.B3}
 				>
-					<Square icon={GameProps.boardCells.b3} />
+					<Square icon={gameState.boardCells.b3} />
 				</Styled.BoardCol>
 			</Styled.BoardRow>
 			<Styled.BoardRow>
@@ -134,21 +115,21 @@ const Board = () => {
 					onClick={handleClick}
 					data-id={CellEnum.C1}
 				>
-					<Square icon={GameProps.boardCells.c1} />
+					<Square icon={gameState.boardCells.c1} />
 				</Styled.BoardCol>
 				<Styled.BoardCol
 					xs={4}
 					onClick={handleClick}
 					data-id={CellEnum.C2}
 				>
-					<Square icon={GameProps.boardCells.c2} />
+					<Square icon={gameState.boardCells.c2} />
 				</Styled.BoardCol>
 				<Styled.BoardCol
 					xs={4}
 					onClick={handleClick}
 					data-id={CellEnum.C3}
 				>
-					<Square icon={GameProps.boardCells.c3} />
+					<Square icon={gameState.boardCells.c3} />
 				</Styled.BoardCol>
 			</Styled.BoardRow>
 		</Styled.BoardContainer>
---
+++ src/components/Scoreboard.styles.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+
+import { Container } from 'react-bootstrap'
+
+export const ScoreBoardContainer = styled(Container)`
+	background-color: #95b8d1;
+`
